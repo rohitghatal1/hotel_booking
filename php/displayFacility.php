849a0876af6cc986aa1fbc1115790b205baf05b0
rohitghatal1 <rohitghatal@gmail.com>
2024-04-17 01:52:06 -0700

    changed style for facilities and adjusted footer

--- php/displayFacility.php
+++ php/displayFacility.php
@@ -4,8 +4,7 @@ $facilityDisplay = $conn->query($getFacility);
 if ($facilityDisplay->num_rows > 0) {
     while ($row = $facilityDisplay->fetch_assoc()) {
         echo "<div class='facility-items'>";
-        echo "<div class='border-top'></div>";
-        echo "<img src='{$row['pic']}' alt=''>";
+        echo "<div class='facilityImage'><img src='{$row['pic']}' alt=''></div>";
         echo "<h4>{$row['facilityName']}</h4>";
         echo "<p>{$row['aboutFacility']}</p>";
         echo "</div>";
